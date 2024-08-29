fd06e59cc6e34dd288f12c59921009cff0da5809
Michal Trybus <komar007@gmail.com>
2024-08-29 22:43:54 +0200

    multiple pid support

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use std::{
 
 use daemonize::Daemonize;
 use fork::Fork;
+use futures::{stream::unfold, StreamExt};
+use itertools::Itertools;
 use log::{debug, error, info};
 use procfs::{CurrentSI, KernelStats};
 use tokio::{
     fs,
-    io::{AsyncReadExt, AsyncWriteExt, BufStream},
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, BufStream, BufWriter},
     net::{UnixListener, UnixStream as TokioUnixStream},
+    pin, stream,
     sync::RwLock,
 };
 
@@ -22,9 +25,10 @@ const DAEMON_RETRY_DELAY: u64 = 10;
 const SOCKET_FILENAME: &str = "/tmp/fire.sock";
 const PID_FILENAME: &str = "/tmp/fire.pid";
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), ()> {
     env_logger::init();
-    let (mut stream_child, stream_parent) = UnixStream::pair()?;
+    let (mut stream_child, stream_parent) =
+        UnixStream::pair().map_err(|e| error!("could not create UnixStream pair: {e}"))?;
     match fork::fork() {
         Ok(Fork::Child) => {
             info!("child process");
@@ -33,51 +37,75 @@ fn main() -> std::io::Result<()> {
             match daemonize.start() {
                 Ok(_) => {
                     info!("daemonized");
-                    run_daemon(stream_child)?;
+                    run_daemon(stream_child).map_err(|e| error!("{}", e))
                 }
                 Err(e) => {
                     stream_child
                         .write_all(&(ReadyToken::DaemonRunning as u32).to_be_bytes())
-                        .inspect_err(|e| error!("error writing to parent: {e}"))?;
+                        .map_err(|e| error!("error writing to parent: {e}"))?;
                     debug!("error deamonizing: {}, assuming daemon running", e);
+                    Ok(())
                 }
-            };
+            }
         }
         Ok(Fork::Parent(_)) => {
             drop(stream_child);
-            run_client(stream_parent)?;
+            run_client(stream_parent).map_err(|e| {
+                e.inspect(|e| error!("{}", e));
+            })
         }
         Err(_) => {
             error!("couldn't fork");
+            Err(())
         }
     }
-    Ok(())
 }
 
 #[tokio::main]
-async fn run_client(other_fork: UnixStream) -> std::io::Result<()> {
-    other_fork.set_nonblocking(true)?;
+async fn run_client(other_fork: UnixStream) -> Result<(), Option<String>> {
+    other_fork
+        .set_nonblocking(true)
+        .map_err(|e| format!("could not set UnixStream nonblocking: {e}"))?;
     let mut other_fork = TokioUnixStream::from_std(other_fork)
-        .inspect_err(|e| error!("error tokioing UnixStream to fork: {e}"))?;
+        .map_err(|e| format!("error tokioing UnixStream to fork: {e}"))?;
     let ready = other_fork
         .read_u32()
         .await
-        .inspect_err(|e| error!("error reading from fork parent: {e}"))?;
+        .map_err(|e| format!("error reading from fork parent: {e}"))?;
     let ready: ReadyToken =
         num::FromPrimitive::from_u32(ready).expect("expected known value of ready token");
     info!("parent ready, {:?}, starting client", ready);
     let mut stream = connect_to_daemon(ready).await;
-    let pid: i32 = args().nth(1).unwrap().parse().unwrap();
-    stream
-        .write_i32(pid)
-        .await
-        .inspect_err(|e| error!("error writing to server: {e}"))?;
+    let pids: Result<Vec<i32>, _> = args().skip(1).map(|a| a.parse()).collect();
+    let pids = pids.map_err(|_| {
+        print_usage();
+        None
+    })?;
+    for pid in &pids {
+        stream
+            .write_i32(*pid)
+            .await
+            .map_err(|e| format!("error writing to server: {e}"))?;
+    }
     stream
         .flush()
         .await
-        .inspect_err(|e| error!("error flushing stream: {e}"))?;
-    while let Ok(resp) = stream.read_f32().await {
-        println!("{resp}");
+        .map_err(|e| format!("error flushing stream: {e}"))?;
+    stream
+        .shutdown()
+        .await
+        .map_err(|e| format!("error shutting down stream: {e}"))?;
+    let loads_stream = unfold(stream, |mut stream| async {
+        stream.read_f32().await.ok().map(|load| (load, stream))
+    })
+    .chunks(pids.len());
+    pin!(loads_stream);
+    while let Some(loads) = loads_stream.next().await {
+        let sum: f32 = loads
+            .iter()
+            .map(|l| if l.is_nan() { 0.0 } else { *l })
+            .sum();
+        println!("{} {}", sum, loads.iter().format(" "))
     }
     Ok(())
 }
@@ -99,6 +127,11 @@ async fn connect_to_daemon(ready: ReadyToken) -> TokioUnixStream {
     stream
 }
 
+fn print_usage() {
+    let name = args().next().expect("args should have program name");
+    println!("usage:\n\t{name} pid [..pids]")
+}
+
 #[derive(Debug, FromPrimitive)]
 enum ReadyToken {
     DaemonForked = 0x4ea11e55,
@@ -106,17 +139,19 @@ enum ReadyToken {
 }
 
 #[tokio::main]
-async fn run_daemon(other_fork: UnixStream) -> std::io::Result<()> {
-    other_fork.set_nonblocking(true)?;
+async fn run_daemon(other_fork: UnixStream) -> Result<(), String> {
+    other_fork
+        .set_nonblocking(true)
+        .map_err(|e| format!("could not set UnixStream nonblocking: {e}"))?;
     let mut other_fork = TokioUnixStream::from_std(other_fork)
-        .inspect_err(|e| error!("error tokioing UnixStream to fork: {e}"))?;
+        .map_err(|e| format!("error tokioing UnixStream to fork: {e}"))?;
     let _ = fs::remove_file(SOCKET_FILENAME).await;
     let listener = UnixListener::bind(SOCKET_FILENAME).expect("should create socket");
 
     other_fork
         .write_u32(ReadyToken::DaemonForked as u32)
         .await
-        .inspect_err(|e| error!("error reading from fork parent: {e}"))?;
+        .map_err(|e| format!("error reading from fork parent: {e}"))?;
 
     let loads: HashMap<i32, f32> = HashMap::new();
     let loads = Arc::new(RwLock::new(loads));
@@ -125,32 +160,43 @@ async fn run_daemon(other_fork: UnixStream) -> std::io::Result<()> {
         match listener.accept().await {
             Ok((socket, addr)) => {
                 info!("accepted from {addr:?}");
-                let loads = loads.clone();
-                tokio::spawn(async move {
-                    let mut stream = BufStream::new(socket);
-                    let Ok(pid) = stream.read_i32().await else {
-                        error!("error reading request");
-                        return;
-                    };
-                    loop {
-                        let response = loads.read().await.get(&pid).copied().unwrap_or(-1.0);
-                        if let Err(e) = stream.write_f32(response).await {
-                            error!("error writing response: {e}");
-                            break;
-                        }
-                        if let Err(e) = stream.flush().await {
-                            error!("error flushing stream: {e}");
-                            break;
-                        }
-                        tokio::time::sleep(Duration::from_millis(UPDATE_INTERVAL_MS)).await;
-                    }
-                    if let Err(e) = stream.shutdown().await {
-                        error!("error shutting down: {e}");
-                    }
-                });
+                tokio::spawn(serve_client(socket, loads.clone()));
             }
             Err(e) => error!("accept function failed: {:?}", e),
         }
+    }
+}
+
+async fn serve_client(mut socket: TokioUnixStream, loads: Arc<RwLock<HashMap<i32, f32>>>) {
+    let (reader, writer) = socket.split();
+    let reader = BufReader::new(reader);
+    let mut writer = BufWriter::new(writer);
+    let pids: Vec<_> = unfold(reader, |mut reader| async {
+        reader.read_i32().await.ok().map(|pid| (pid, reader))
+    })
+    .collect()
+    .await;
+    'infinite: loop {
+        let pid_loads: Vec<_> = {
+            let loads = loads.read().await;
+            pids.iter()
+                .map(|pid| *loads.get(pid).unwrap_or(&f32::NAN))
+                .collect()
+        };
+        for pid in pid_loads {
+            if let Err(e) = writer.write_f32(pid).await {
+                error!("error writing response: {e}");
+                break 'infinite;
+            }
+        }
+        if let Err(e) = writer.flush().await {
+            error!("error flushing stream: {e}");
+            break;
+        }
+        tokio::time::sleep(Duration::from_millis(UPDATE_INTERVAL_MS)).await;
+    }
+    if let Err(e) = socket.shutdown().await {
+        error!("error shutting down: {e}");
     }
 }
 
